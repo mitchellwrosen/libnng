341b857e162ed7567a47f4bd45637a72d4eec2a6
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-09-18 00:58:36 -0400

    few basic functions

--- src/Libnng.hs
+++ src/Libnng.hs
@@ -1,33 +1,190 @@
-{-# LANGUAGE CPP                      #-}
-{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE BlockArguments             #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE ForeignFunctionInterface   #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
 
 module Libnng
-  ( strerror
-  , version
+  ( NngDialer(..)
+  , NngListener(..)
+  , NngSocket(..)
+  , nng_close
+  , nng_dial
+  , nng_dialer_close
+  , nng_listen
+  , nng_rep0_open
+  , nng_req0_open
+  , nng_strerror
+  , nng_version
   ) where
 
 import Data.ByteString (ByteString)
 import Data.Text (Text)
+import Data.Word (Word32)
 import Foreign.C.String
+import Foreign.Marshal.Alloc
+import Foreign.Ptr
+import Foreign.Storable
 import System.IO.Unsafe (unsafePerformIO)
 import qualified Data.ByteString as ByteString
 import qualified Data.Text.Encoding as Text
 
-foreign import ccall "nng_strerror" nng_strerror
+
+newtype NngDialer
+  = NngDialer { unNngDialer :: Word32 }
+  deriving stock ( Eq, Show )
+  deriving newtype ( Storable )
+
+newtype NngListener
+  = NngListener { unNngListener :: Word32 }
+  deriving stock ( Eq, Show )
+  deriving newtype ( Storable )
+
+newtype NngSocket
+  = NngSocket { unNngSocket :: Word32 }
+  deriving stock ( Eq, Show )
+  deriving newtype ( Storable )
+
+
+nng_close
+  :: NngSocket
+  -> IO Int
+nng_close =
+  c_nng_close
+
+nng_dial
+  :: NngSocket
+  -> Text
+  -> Int
+  -> IO ( Either Int NngDialer )
+nng_dial socket url flags =
+  ByteString.useAsCString
+    ( Text.encodeUtf8 url )
+    \url2 ->
+      allocaBytes 4 \dialerPtr -> do
+        result :: Int <-
+          c_nng_dial
+            socket
+            url2
+            dialerPtr
+            flags
+
+        if result == 0
+          then
+            Right <$> peek dialerPtr
+
+          else
+            pure ( Left result )
+
+nng_dialer_close
+  :: NngDialer
+  -> IO Int
+nng_dialer_close =
+  c_nng_dialer_close
+
+nng_listen
+  :: NngSocket
+  -> Text
+  -> Int
+  -> IO ( Either Int NngListener )
+nng_listen socket url flags =
+  ByteString.useAsCString
+    ( Text.encodeUtf8 url )
+    \url2 ->
+      allocaBytes 4 \listenerPtr -> do
+        result :: Int <-
+          c_nng_listen
+            socket
+            url2
+            listenerPtr
+            flags
+
+        if result == 0
+          then
+            Right <$> peek listenerPtr
+
+          else
+            pure ( Left result )
+
+nng_rep0_open
+  :: IO ( Either Int NngSocket )
+nng_rep0_open =
+  allocaBytes 4 \socketPtr -> do
+    result :: Int <-
+      c_nng_rep0_open socketPtr
+
+    if result == 0
+      then
+        Right <$> peek socketPtr
+
+      else
+        pure ( Left result )
+
+nng_req0_open
+  :: IO ( Either Int NngSocket )
+nng_req0_open =
+  allocaBytes 4 \socketPtr -> do
+    result :: Int <-
+      c_nng_req0_open socketPtr
+
+    if result == 0
+      then
+        Right <$> peek socketPtr
+
+      else
+        pure ( Left result )
+
+nng_strerror
+  :: Int
+  -> Text
+nng_strerror =
+  Text.decodeUtf8 . unsafePerformIO . ByteString.packCString . c_nng_strerror
+{-# NOINLINE nng_strerror #-}
+
+nng_version :: ByteString
+nng_version =
+  unsafePerformIO ( ByteString.packCString c_nng_version )
+{-# NOINLINE nng_version #-}
+
+
+--------------------------------------------------------------------------------
+-- Foreign imports
+-- TODO safe, unsafe, wha?
+--------------------------------------------------------------------------------
+
+foreign import ccall "nng_close" c_nng_close
+  :: NngSocket
+  -> IO Int
+
+foreign import ccall "nng_dial" c_nng_dial
+  :: NngSocket
+  -> CString
+  -> Ptr NngDialer
+  -> Int
+  -> IO Int
+
+foreign import ccall "nng_dialer_close" c_nng_dialer_close
+  :: NngDialer
+  -> IO Int
+
+foreign import ccall "nng_listen" c_nng_listen
+  :: NngSocket
+  -> CString
+  -> Ptr NngListener
+  -> Int
+  -> IO Int
+
+foreign import ccall "nng_rep0_open" c_nng_rep0_open
+  :: Ptr NngSocket
+  -> IO Int
+
+foreign import ccall "nng_req0_open" c_nng_req0_open
+  :: Ptr NngSocket
+  -> IO Int
+
+foreign import ccall "nng_strerror" c_nng_strerror
   :: Int
   -> CString
 
-foreign import ccall "nng_version" nng_version
+foreign import ccall "nng_version" c_nng_version
   :: CString
-
-strerror
-  :: Int
-  -> Text
-strerror =
-  Text.decodeUtf8 . unsafePerformIO . ByteString.packCString . nng_strerror
-{-# NOINLINE strerror #-}
-
-version :: ByteString
-version =
-  unsafePerformIO ( ByteString.packCString nng_version )
-{-# NOINLINE version #-}
---
+++ test/Main.hs
@@ -0,0 +1,208 @@
+{-# LANGUAGE BlockArguments      #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DerivingStrategies  #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import Control.Applicative
+import Control.Exception
+import Control.Monad
+import Data.Foldable
+import Data.IORef
+import Data.Set (Set)
+import System.Exit
+import System.IO.Unsafe (unsafePerformIO)
+import qualified Data.Set as Set
+
+import Libnng
+
+passed :: IORef ( Set String )
+passed =
+  unsafePerformIO ( newIORef Set.empty )
+{-# NOINLINE passed #-}
+
+main :: IO ()
+main = do
+  do
+    names <-
+      Set.fromList . lines <$>
+        ( readFile ".testcache" <|> pure "" )
+    writeIORef passed names
+
+  test "nng_close returns 7 on bogus socket" do
+    nng_close ( NngSocket 0 ) `shouldReturn` 7
+
+  test "nng_close returns 0 on open socket" do
+    socket <- shouldReturnRight nng_req0_open
+    nng_close socket `shouldReturn` 0
+
+  test "nng_close returns 7 on closed socket" do
+    socket <- shouldReturnRight nng_req0_open
+    nng_close socket `shouldReturn` 0
+    nng_close socket `shouldReturn` 7
+
+  test "nng_dial returns Left 3 on bogus url" do
+    with_nng_req0 \socket ->
+      nng_dial socket "foo" 0 `shouldReturn` Left 3
+
+  test "nng_dial returns Left 6 on endpoint that's not listening" do
+    with_nng_req0 \socket ->
+      nng_dial socket "inproc://foo" 0 `shouldReturn` Left 6
+
+  test "nng_dial returns Left 7 on bogus socket" do
+    nng_dial ( NngSocket 0 ) "inproc://foo" 0 `shouldReturn` Left 7
+
+  -- TODO nng_dial protocol error
+
+  test "nng_dial returns Right" do
+    with_nng_req0 \req ->
+      with_nng_rep0 \rep -> do
+        _ <- shouldReturnRight ( nng_listen rep "inproc://foo" 0 )
+        _ <- shouldReturnRight ( nng_dial req "inproc://foo" 0 )
+        pure ()
+
+  test "nng_dialer_close returns 12 on bogus dialer" do
+    nng_dialer_close ( NngDialer 0 ) `shouldReturn` 12
+
+  test "nng_dialer_close returns 0 on open dialer" do
+    with_nng_req0 \req ->
+      with_nng_rep0 \rep -> do
+        _ <- shouldReturnRight ( nng_listen rep "inproc://foo" 0 )
+        dialer <- shouldReturnRight ( nng_dial req "inproc://foo" 0 )
+        nng_dialer_close dialer `shouldReturn` 0
+
+  test "nng_listen returns Left 3 on bogus url" do
+    with_nng_rep0 \socket ->
+      nng_listen socket "foo" 0 `shouldReturn` Left 3
+
+  test "nng_listen returns Left 7 on bogus socket" do
+    nng_listen ( NngSocket 0 ) "inproc://foo" 0 `shouldReturn` Left 7
+
+  test "nng_listen returns Right" do
+    with_nng_rep0 \socket -> do
+      _ <- shouldReturnRight ( nng_listen socket "inproc://foo" 0 )
+      pure ()
+
+  -- TODO nng_listen protocol error
+
+  test "nng_rep0_open returns Right" do
+    socket <- shouldReturnRight nng_rep0_open
+    nng_close socket `shouldReturn` 0
+
+  test "nng_req0_open returns Right" do
+    socket <- shouldReturnRight nng_req0_open
+    nng_close socket `shouldReturn` 0
+
+  traverse_
+    ( \(n, s) ->
+        test ( "nng_strerror " ++ show n ) do
+          nng_strerror n `shouldBe` s
+    )
+    [ ( 0,  "Hunky dory"         )
+    , ( 1,  "Interrupted"        )
+    , ( 2,  "Out of memory"      )
+    , ( 3,  "Invalid argument"   )
+    , ( 4,  "Resource busy"      )
+    , ( 5,  "Timed out"          )
+    , ( 6,  "Connection refused" )
+    , ( 7,  "Object closed"      )
+    , ( 8,  "Try again"          )
+    , ( 9,  "Not supported"      )
+    , ( 10, "Address in use"     )
+    , ( 11, "Incorrect state"    )
+    , ( 12, "Entry not found"    )
+      -- Well these are boring to test!
+    ]
+
+  test "nng_version is 1.1.1" do
+    nng_version `shouldBe` "1.1.1"
+
+  do
+    names <- readIORef passed
+    writeFile ".testcache" ( unlines ( Set.toList names ) )
+
+with_nng_rep0
+  :: ( NngSocket -> IO a )
+  -> IO a
+with_nng_rep0 action =
+  bracket
+    nng_rep0_open
+    ( either
+        ( const ( pure () ) )
+        ( void . nng_close )
+    )
+    ( either
+        ( \n -> throwIO ( userError ( "with_nng_rep0: " ++ show n ) ) )
+        action
+    )
+
+with_nng_req0
+  :: ( NngSocket -> IO a )
+  -> IO a
+with_nng_req0 action =
+  bracket
+    nng_req0_open
+    ( either
+        ( const ( pure () ) )
+        ( void . nng_close )
+    )
+    ( either
+        ( \n -> throwIO ( userError ( "with_nng_req0: " ++ show n ) ) )
+        action
+    )
+
+test
+  :: String
+  -> IO ()
+  -> IO ()
+test name action = do
+  names <- readIORef passed
+
+  if Set.member name names
+    then
+      putStrLn ( "-- " ++ name )
+    else do
+      putStrLn name
+      try action >>= \case
+        Left ( _ :: SomeException ) -> pure ()
+        Right () -> modifyIORef' passed ( Set.insert name )
+
+failure
+  :: String
+  -> IO a
+failure message = do
+  putStrLn ( "  ✗ " ++ message )
+  exitFailure
+
+shouldBe
+  :: ( Eq a, Show a )
+  => a
+  -> a
+  -> IO ()
+shouldBe x y =
+  unless ( x == y )
+    ( failure ( show x ++ " /= " ++ show y ) )
+
+shouldReturn
+  :: ( Eq a, Show a )
+  => IO a
+  -> a
+  -> IO ()
+shouldReturn mx y = do
+  x <- mx
+  x `shouldBe` y
+
+shouldReturnRight
+  :: Show a
+  => IO ( Either a b )
+  -> IO b
+shouldReturnRight action =
+  action >>= \case
+    Left x ->
+      failure ( "Left " ++ show x )
+
+    Right y ->
+      pure y
